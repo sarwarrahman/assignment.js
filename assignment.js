111511befe5ce708e442f6d2aab1f5f8aa78f6aa
sarwarrahman <sarwarrahman000@gmail.com>
2020-07-17 21:27:10 +0600

    finished assignment.js

---
+++ assignment.js
@@ -0,0 +1,56 @@
+//feetToMile
+function feetToMile(feet) {
+    var mile = feet/5280;
+    return mile;
+}
+var senior = [10560, 15840, 21120];
+
+var nanaMile = feetToMile(senior[0]);
+console.log(nanaMile);
+var naniMile = feetToMile(15840);
+console.log(naniMile);
+var dadiMile = feetToMile(21120);
+console.log(dadiMile);
+
+
+//woodCalculator
+var chair = 3;
+var table = 6;
+var bed = 9;
+var total = chair + table + bed;
+console.log(total);
+
+var chair14 = chair * 14;
+var table3 = table * 3;
+var bed5 = bed * 5;
+var total2 = chair14 + table3 + bed5;
+console.log(total2);
+
+
+
+//brickCalculator
+const brick = 1000;
+var floor1st = brick * 15;
+var floor2nd = brick * 15;
+var floor3rd = brick * 15;
+var floor4th = brick * 15;
+var floor5th = brick * 15;
+var floor6th = brick * 15;
+var floor7th = brick * 15;
+var floor8th = brick * 15;
+var floor9th = brick * 15;
+var floor10th = brick * 12;
+var floor11th = brick * 12;
+var floor12th = brick * 12;
+var floor13th = brick * 12;
+var floor14th = brick * 12;
+var floor15th = brick * 12;
+var total = floor1st + floor2nd + floor3rd + floor4th + floor5th + floor6th + floor7th + floor8th + floor9th + floor10th + floor11th + floor12th + floor13th + floor14th + floor15th
+console.log(total);
+
+
+//tinyFriend
+var friendName = ['Sarwar', 'Sarmi', 'Ayan'];
+var tinyFriend = friendName[2];
+console.log(tinyFriend);
+
